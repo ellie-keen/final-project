94e4ae4bd48b9a34f1d2200b22079c39e0608daf
Mark Channer <markchanner@gmail.com>
2018-06-27 13:45:44 +0100

    Rename cols to rows

--- src/buttonGrid.js
+++ src/buttonGrid.js
@@ -2,12 +2,12 @@ function ButtonGrid(context) {
   this.context = context;
 }
 
-ButtonGrid.prototype.create = function(cols, x, y) {
+ButtonGrid.prototype.create = function(rows, x, y) {
   let width = 20;
   let height = 20;
   let gap = height / 4;
 
-  for (let i = 0; i < cols; i++) {
+  for (let i = 0; i < rows; i++) {
     this.context.fillStyle = 'blue';
     this.context.fillRect(x, y, width, height);
     y += height + gap;
