f173c8c4ddc210b53d9ae3474fcf72091524f3fa
Asia Antczak <aasiaantczak@gmail.com>
2018-06-28 13:32:03 +0100

    Change fillRect arguments

--- src/gridView.js
+++ src/gridView.js
@@ -6,7 +6,7 @@ GridView.prototype.drawGrid = function (context, buttonGrid) {
     for (let j = 0; j < buttonGrid.rows; j++) {
       let button = buttonGrid.buttons[i][j]
       context.fillStyle = button.color
-      context.fillRect((i * button.size) + button.size, (j * button.size) + button.size, button.size, button.size)
+      context.fillRect((i * button.size) * 1.5, (j * button.size) * 1.5, button.size, button.size)
     }
   }
 };
