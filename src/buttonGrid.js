934168134b6745d0ca1de64bdf9596520b6e31f8
Mark Channer <markchanner@gmail.com>
2018-06-28 16:03:36 +0100

    Remove size attribute

--- src/buttonGrid.js
+++ src/buttonGrid.js
@@ -8,7 +8,7 @@ ButtonGrid.prototype.init = function() {
   for (let i = 0; i < this.rows; i++) {
     var buttonRow = [];
     for (let j = 0; j < this.cols; j++) {
-      buttonRow.push(new Button('blue', 20));
+      buttonRow.push(new Button('blue'));
     }
     this.buttons.push(buttonRow);
   }
