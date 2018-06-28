0a5ce60ca23680214d992c6a4797c4500ea34dc3
Asia Antczak <aasiaantczak@gmail.com>
2018-06-28 13:07:01 +0100

    Add GridView

--- spec/gridViewSpec.js
+++ spec/gridViewSpec.js
@@ -1,12 +1,13 @@
-// describe("GridView", function() {
-//
-//   var gridView = new GridView();
-//
-//   it("draws the 5 x 5 grid", function() {
-//     let context = jasmine.createSpyObj('context', ['fillRect']);
-//     let buttonGrid = new ButtonGrid()
-//     gridView.drawGrid(context, buttonGrid);
-//     expect(context.fillRect).toHaveBeenCalled();
-//     // expect(context.fillRect).toHaveBeenCalledTimes(25);
-//   });
-// })
+describe("GridView", function() {
+
+  var gridView = new GridView();
+
+  it("draws the 5 x 5 grid", function() {
+    let context = jasmine.createSpyObj('context', ['fillRect']);
+    let buttonGrid = new ButtonGrid(5,5)
+    buttonGrid.init();
+    gridView.drawGrid(context, buttonGrid);
+    expect(context.fillRect).toHaveBeenCalled();
+    expect(context.fillRect).toHaveBeenCalledTimes(25);
+  });
+})
--- src/gridView.js
+++ src/gridView.js
@@ -1,6 +1,12 @@
-// function GridView() {
-// }
-//
-// GridView.prototype.drawGrid = function (context, buttonGrid) {
-//   for (let i = 0; i < )
-// };
+function GridView() {
+}
+
+GridView.prototype.drawGrid = function (context, buttonGrid) {
+  for (let i = 0; i < buttonGrid.cols; i++) {
+    for (let j = 0; j < buttonGrid.rows; j++) {
+      let button = buttonGrid.buttons[i][j]
+      context.fillStyle = button.color
+      context.fillRect((i * button.size) + button.size, (j * button.size) + button.size, button.size, button.size)
+    }
+  }
+};
