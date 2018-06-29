eb41f59b11f539b4a32a865bcdb6b40195d195ef
Ellie Keen <elliekeen95@gmail.com>
2018-06-29 15:21:42 +0100

    Add toggle method to Button

--- spec/buttonSpec.js
+++ spec/buttonSpec.js
@@ -1,5 +1,10 @@
 describe('Button', function() {
-  let button = new Button('blue', 1);
+
+  var button;
+
+  beforeEach(function() {
+    button = new Button('blue', 1);
+  });
 
   it('has a color', function() {
     let color = 'blue';
@@ -9,5 +14,20 @@ describe('Button', function() {
   it('has a frequency', function() {
     let frequency = 1
     expect(button.frequency).toEqual(1);
-  })
+  });
+
+  it('is off when initialized', function() {
+    expect(button.isOn).toEqual(false);
+  });
+
+  it('can be turned on', function() {
+    button.toggle();
+    expect(button.isOn).toEqual(true);
+  });
+
+  it('can be turned off', function() {
+    button.toggle();
+    button.toggle();
+    expect(button.isOn).toEqual(false);
+  });
 });
--- src/button.js
+++ src/button.js
@@ -1,4 +1,9 @@
 function Button(color, frequency) {
   this.color = color;
   this.frequency = frequency;
+  this.isOn = false;
 }
+
+Button.prototype.toggle = function() {
+  this.isOn = !this.isOn;
+};
