ed2a9128aa5e1041f2517526845a5b096ab706f0
Mark Channer <markchanner@gmail.com>
2018-06-29 12:41:13 +0100

    Refactor clickIsWithinButton and change grid size

--- src/gridView.js
+++ src/gridView.js
@@ -1,6 +1,6 @@
 function GridView(context) {
   this.gap = 1.5;
-  this.buttonSize = 20;
+  this.buttonSize = 30;
   this.context = context;
 }
 
@@ -20,23 +20,29 @@ GridView.prototype.drawGrid = function(buttonGrid) {
 };
 
 GridView.prototype.update = function(buttonGrid, x, y) {
-  if (this.clickIsWithinButton(x, y)) {
+  if (this.clickIsWithinButton(buttonGrid, x, y)) {
     var row = Math.floor(y / this.buttonSize / this.gap);
     var col = Math.floor(x / this.buttonSize / this.gap);
     var button = buttonGrid.buttons[row][col];
     button.color = 'red';
+    console.log(buttonGrid);
   }
 };
 
-GridView.prototype.clickIsWithinButton = function(x, y) {
-  return !(x > 20 && x < 30 ||
-          x > 50 && x < 60 ||
-          x > 80 && x < 90 ||
-          x > 110 && x < 120 ||
-          x > 140 ||
-          y > 20 && y < 30 ||
-          y > 50 && y < 60 ||
-          y > 80 && y < 90 ||
-          y > 110 && y < 120 ||
-          y > 140)
+GridView.prototype.clickIsWithinButton = function(buttonGrid, x, y) {
+  for (let col = 0; col < buttonGrid.cols; col++) {
+    for (let row = 0; row < buttonGrid.rows; row++) {
+      let buttonX = col * this.buttonSize * this.gap;
+      let buttonY = row * this.buttonSize * this.gap;
+      if (
+        x >= buttonX &&
+        x <= buttonX + this.buttonSize &&
+        y >= buttonY &&
+        y <= buttonY + this.buttonSize
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
