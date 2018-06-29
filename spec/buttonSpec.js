9b4b726ce43d4c396a8b4cfc132282b869deebba
Asia Antczak <aasiaantczak@gmail.com>
2018-06-29 12:53:11 +0100

    Add frequency to a button

--- spec/buttonSpec.js
+++ spec/buttonSpec.js
@@ -1,8 +1,13 @@
 describe('Button', function() {
-  let button = new Button('blue');
+  let button = new Button('blue', 1);
 
   it('has a color', function() {
     let color = 'blue';
     expect(button.color).toEqual(color);
   });
+
+  it('has a frequency', function() {
+    let frequency = 1
+    expect(button.frequency).toEqual(1);
+  })
 });
