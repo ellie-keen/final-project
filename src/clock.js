d92635908b768551e8802f4fe356045e4454d701
Anders Persson <35977567+anderscodes@users.noreply.github.com>
2018-06-29 17:00:06 +0100

    Update clock.js

--- src/clock.js
+++ src/clock.js
@@ -4,10 +4,11 @@ function Clock() {
 
 Clock.prototype.interval = function(buttonGrid) {
   var self = this;
+  var intervalDuration = 1000;
   setInterval(function() {
     self.step === buttonGrid.cols - 1 ? self.step = 0 : self.step++;
     for (i = 0; i < buttonGrid.rows; i++){
      playSound(buttonGrid.buttons[i][self.step]);
     }
-  },1000)
+  },intervalDuration)
 }
