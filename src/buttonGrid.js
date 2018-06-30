d4433f581cf8a7cfedd1035a850066f2292bad49
Asia Antczak <aasiaantczak@gmail.com>
2018-06-30 14:00:04 +0100

    Change frequency for each buttons' row

--- src/buttonGrid.js
+++ src/buttonGrid.js
@@ -5,18 +5,13 @@ function ButtonGrid(rows, cols) {
 }
 
 ButtonGrid.prototype.init = function() {
+  var frequency = 80;
   for (let row = 0; row < this.rows; row++) {
     var buttonRow = [];
     for (let col = 0; col < this.cols; col++) {
-      var frequency;
-      if (row % 2 == 0) {
-        frequency = 440;
-      } else {
-        frequency = 392;
-      }
       buttonRow.push(new Button(frequency));
     }
+    frequency += 30
     this.buttons.push(buttonRow);
   }
-  console.log(this.buttons);
 };
