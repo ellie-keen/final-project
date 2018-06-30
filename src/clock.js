c05d1414b119cb546f13de245248adc52802731e
Asia Antczak <aasiaantczak@gmail.com>
2018-06-30 14:30:23 +0100

    Mark play function on clock private

--- src/clock.js
+++ src/clock.js
@@ -4,15 +4,15 @@ function Clock() {
 
 Clock.prototype.interval = function(buttonGrid) {
   var self = this;
-  var intervalDuration = 1000;
+  var intervalDuration = 500;
   setInterval(function() {
     self.step === buttonGrid.cols - 1 ? self.step = 0 : self.step++;
     for (i = 0; i < buttonGrid.rows; i++){
-     self.play(buttonGrid.buttons[i][self.step]);
+     self._play(buttonGrid.buttons[i][self.step]);
     }
   },intervalDuration)
 }
 
-Clock.prototype.play = function(button) {
+Clock.prototype._play = function(button) {
   button.isOn ? playSound(audio, button.frequency, "sine") : null;
 }
