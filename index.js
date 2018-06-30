df6ff567ebd74075ff4b94614ab4b04b418f193a
Ellie Keen <elliekeen95@gmail.com>
2018-06-30 14:21:41 +0100

    Add Express server

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var static = require('node-static');
-var file = new static.Server('./index.html');
+var express = require('express');
+var app = express();
+var path = require('path');
 
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+// use port 3000 unless there exists a preconfigured port
+var port = process.env.port || 3000;
+app.listen(port, () => console.log('Listening on port 3000!'));
