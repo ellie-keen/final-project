49512f3e349653d15983a9880d85ead7c0b94de6
Ellie Keen <elliekeen95@gmail.com>
2018-06-30 16:13:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var port = (process.env.port || 3000);
+
 app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', function(req, res) {
@@ -9,5 +11,4 @@ app.get('/', function(req, res) {
 });
 
 // use port 3000 unless there exists a preconfigured port
-var port = process.env.port || 3000;
-app.listen(port, () => console.log('Listening on port 3000!'));
+app.listen(port, () => console.log(`Listening on port ${port}));
