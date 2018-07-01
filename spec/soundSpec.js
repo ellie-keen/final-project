93ff66e02137c20ab2218faa5782fd8bb9087793
Mark Channer <markchanner@gmail.com>
2018-07-01 20:07:51 +0100

    Prepend underscore to private method names

--- spec/soundSpec.js
+++ spec/soundSpec.js
@@ -9,23 +9,23 @@ describe('Sound', function() {
 
   describe('createWave', function() {
     it('should return an oscillator object', function() {
-      expect(sound.createWave(2, 'sine')).toEqual(jasmine.any(OscillatorNode));
+      expect(sound._createWave(2, 'sine')).toEqual(jasmine.any(OscillatorNode));
     });
 
     it('should create a sine wave', function() {
-      expect(sound.createWave(2, 'sine').type).toEqual('sine');
+      expect(sound._createWave(2, 'sine').type).toEqual('sine');
     });
   });
 
   describe('note', function() {
     it('should return a function', function() {
-      expect(sound.note(2, 'sine')).toEqual(jasmine.any(Function));
+      expect(sound._note(2, 'sine')).toEqual(jasmine.any(Function));
     });
   });
 
   describe('createAmplifier', function() {
     it('should return an amplifier', function() {
-      expect(sound.createAmplifier(0.2, 2)).toEqual(jasmine.any(GainNode));
+      expect(sound._createAmplifier(0.2, 2)).toEqual(jasmine.any(GainNode));
     });
   });
 });
