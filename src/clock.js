5a9e93f242e844ec418b29fcb03fda210faea9f5
Mark Channer <markchanner@gmail.com>
2018-07-01 19:44:19 +0100

    Take sound argument and call playSound on it

--- src/clock.js
+++ src/clock.js
@@ -1,18 +1,19 @@
-function Clock() {
+function Clock(sound) {
   this.step = 0;
+  this.sound = sound;
 }
 
 Clock.prototype.interval = function(buttonGrid) {
   var self = this;
   var intervalDuration = 500;
   setInterval(function() {
-    self.step === buttonGrid.cols - 1 ? self.step = 0 : self.step++;
-    for (i = 0; i < buttonGrid.rows; i++){
-     self._play(buttonGrid.buttons[i][self.step]);
+    self.step === buttonGrid.cols - 1 ? (self.step = 0) : self.step++;
+    for (i = 0; i < buttonGrid.rows; i++) {
+      self._play(buttonGrid.buttons[i][self.step]);
     }
-  },intervalDuration)
-}
+  }, intervalDuration);
+};
 
 Clock.prototype._play = function(button) {
-  button.isOn ? playSound(audio, button.frequency, "sine") : null;
-}
+  button.isOn ? this.sound.playSound(button.frequency, 'sine') : null;
+};
