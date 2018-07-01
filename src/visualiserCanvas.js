9e409d9c12e54ff178e27260d4938c5891e58ac3
Mark Channer <markchanner@gmail.com>
2018-07-01 19:44:54 +0100

    Refactor to object-oriented style

--- src/visualiserCanvas.js
+++ src/visualiserCanvas.js
@@ -1,49 +1,37 @@
-var canvas = document.getElementById('visualiserCanvas');
-var visualiserContext = canvas.getContext("2d");
-
-var WIDTH = 800;
-var HEIGHT = 600;
-
-visualiserContext.clearRect(0, 0, WIDTH, HEIGHT);
-
-analyser.fftSize = 2048;
-var bufferLength = analyser.frequencyBinCount;
-var dataArray = new Uint8Array(bufferLength);
-
-function draw() {
-
-  var rValue = Math.floor((Math.random() * 255) + 1);
-  var gValue = Math.floor((Math.random() * 255) + 1);
-  var bValue = Math.floor((Math.random() * 255) + 1);
-  var lineSize = Math.floor((Math.random() * 15) + 1);
-
-  drawVisual = requestAnimationFrame(draw);
-  analyser.getByteTimeDomainData(dataArray);
-
-  visualiserContext.fillStyle = `rgb(255, 255, 255, 0)`;
-  visualiserContext.fillRect(0, 0, WIDTH, HEIGHT);
-  visualiserContext.lineWidth = lineSize;
-  visualiserContext.strokeStyle = `rgb(${rValue}, ${gValue}, ${bValue})`;
-  visualiserContext.beginPath();
-
-  var sliceWidth = WIDTH * 1.0 / bufferLength;
-  var x = 0
-
-  for(var i = 0; i < bufferLength; i++) {
-
-    var v = dataArray[i] /128.0;
-    var y = v * HEIGHT/2;
-
-      if(i === 0) {
-        visualiserContext.moveTo(x, y);
-      } else {
-        visualiserContext.lineTo(x, y);
-      }
-
-      x += sliceWidth;
-      };
-  visualiserContext.lineTo(canvas.width, canvas.height/2);
-    visualiserContext.stroke();
+function Visualiser(canvas, analyser) {
+  this.canvas = canvas;
+  this.context = this.canvas.getContext('2d');
+  this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  this.analyser = analyser;
+  this.analyser.fftSize = 2048;
+  this.bufferLength = this.analyser.frequencyBinCount;
+  this.dataArray = new Uint8Array(this.bufferLength);
 }
 
-draw()
+Visualiser.prototype.draw = function() {
+  var rValue = Math.floor(Math.random() * 255 + 1);
+  var gValue = Math.floor(Math.random() * 255 + 1);
+  var bValue = Math.floor(Math.random() * 255 + 1);
+  var lineSize = Math.floor(Math.random() * 15 + 1);
+
+  this.context.fillStyle = `rgb(255, 255, 255, 0)`;
+  this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  this.context.lineWidth = lineSize;
+  this.context.strokeStyle = `rgb(${rValue}, ${gValue}, ${bValue})`;
+  this.context.beginPath();
+
+  this.analyser.getByteTimeDomainData(this.dataArray);
+
+  var sliceWidth = this.canvas.width * 1.0 / this.bufferLength;
+  var x = 0;
+  for (var i = 0; i < this.bufferLength; i++) {
+    var v = this.dataArray[i] / 128.0;
+    var y = v * this.canvas.height / 2;
+    i == 0 ? this.context.moveTo(x, y) : this.context.lineTo(x, y);
+    x += sliceWidth;
+  }
+
+  this.context.lineTo(this.canvas.width, this.canvas.height / 2);
+  this.context.stroke();
+  requestAnimationFrame(this.draw.bind(this));
+};
