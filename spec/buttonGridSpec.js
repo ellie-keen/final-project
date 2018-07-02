a547632d79ee69ac0c95c0286998d8c34e3825a4
anderscodes <anders.j.persson@hotmail.com>
2018-07-02 14:05:03 +0100

    minor refactor

--- spec/buttonGridSpec.js
+++ spec/buttonGridSpec.js
@@ -12,9 +12,9 @@ describe('ButtonGrid', function() {
 
   it('adds different frequency value for each buttons\' row', function() {
     buttonGrid.init();
-    expect(buttonGrid.buttons[0][0].frequency).toEqual(400);
-    expect(buttonGrid.buttons[0][1].frequency).toEqual(400);
-    expect(buttonGrid.buttons[1][0].frequency).toEqual(360);
-    expect(buttonGrid.buttons[1][1].frequency).toEqual(360);
+    expect(buttonGrid.buttons[0][0].frequency).toEqual(440);
+    expect(buttonGrid.buttons[0][1].frequency).toEqual(440);
+    expect(buttonGrid.buttons[1][0].frequency).toEqual(415);
+    expect(buttonGrid.buttons[1][1].frequency).toEqual(415);
   })
 });
