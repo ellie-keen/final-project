cccd8b99feb3d58a1856f92040694bad136eeb07
anderscodes <anders.j.persson@hotmail.com>
2018-07-02 16:08:21 +0100

    multiple keys implemented

--- spec/keySpec.js
+++ spec/keySpec.js
@@ -1,11 +1,19 @@
 describe('Key', function() {
 
-  var key = new Key();
+  var key
   var a;
+  var b;
   var c;
+  var d;
+  var f;
+
   beforeEach(function() {
+    key = new Key();
     a = [440, 415, 369, 329, 293, 277, 246, 220]
     b = [466, 415, 364, 349, 311, 277, 261, 223]
+    c = [523, 493, 440, 392, 349, 329, 293, 261]
+    d = 587, 523, 493, 440, 392, 369, 329, 293]
+    f = [369, 329, 293, 277, 246, 220, 207, 185]
 
   })
 
@@ -21,4 +29,18 @@ describe('Key', function() {
     key.change()
     expect(key.value).toEqual(b)
   })
+
+  it("changes value when 2nd new key is selected", function() {
+    key.change()
+    key.change()
+    expect(key.value).toEqual(c)
+  })
+
+  it("changes value back to default when 6th key is selected", function() {
+    for (var i = 0; i < 7; i++) {
+      key.change()
+    }
+    expect(key.value).toEqual(d)
+  })
+
 })
--- src/key.js
+++ src/key.js
@@ -4,11 +4,11 @@ function Key() {
       [523, 493, 440, 392, 349, 329, 293, 261],
       [587, 523, 493, 440, 392, 369, 329, 293],
       [369, 329, 293, 277, 246, 220, 207, 185]]
+  this.counter = 0;
   this.value = this.keys[0];
 }
 
 Key.prototype.change = function( ) {
- var counter = 0;
- counter === this.keys.length - 1 ? counter = 0 : counter++;
- this.value = this.keys[counter];
+ this.counter === this.keys.length - 1 ? this.counter = 0 : this.counter++;
+ this.value = this.keys[this.counter];
 }
